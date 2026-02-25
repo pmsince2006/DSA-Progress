d2e3b800b441953cbaedf143c34e37d33f59c796
Prince Mathur <princemathur1596@gmail.com>
2026-02-25 07:24:31 +0530

    Introduction and concepts related to classes and objects
    Added comments explaining class and object concepts in Java.

--- 1.OOPS/J1_Intro_and_Concepts.java
+++ 1.OOPS/J1_Intro_and_Concepts.java
@@ -1,3 +1,39 @@
+// A class is a named group of properties and functions / template / imagination
+
+// creating a class
+// class Student{
+//     int rno;;
+//     float marks;
+// }
+
+
+// object is an instance of a class / real world object of the template or imagination
+
+// class - logical construct
+// object - physical entity (actually occupies space in the memory)
+// objects are stored in heap memory and referance variables in stack memory
+
+// object properties:
+// instance variables are the variables in the class, we can link the object variable and instance variable using . operator
+
+
+// new operator dynamically allocates memory and returns a referance variable to it
+// hence all class objects in java must be allocated dynamically
+
+// Dynamic memory allocation:
+// Student st1      =   new Student(); (object allocated dynamically !)
+// (compile time) < - > (runtime)
+
+
+// When we try to print the property of a instance variable, it checks if there exits a objects or prints the DEFAULT VALUES
+
+// constructors defines what happens when the object will be created (allocation)
+// it is a special function when it runs when object is created and it allocates some vairbles
+// properties in the class will be diffrent for every refrence variable, which is resolved by this keyword
+
+// primitves are stored in java, the objects in the heap, is the reason why we dont use new keyword while declaring variables
+
+//using finalize we can specify the actions to be performed when java does the garbage collection
 public class J1_Intro_and_Concepts {
     public static void main(String[] args) {
         
