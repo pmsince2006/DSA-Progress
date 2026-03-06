a11edb356040cc264ff24595a51665ab1e5e2094
Prince Mathur <princemathur1596@gmail.com>
2026-03-06 22:29:28 +0530

    Add files via upload

---
+++ 2.BitManipulation/J2_Basic_Bitmanipulation_Questions.java
@@ -0,0 +1,48 @@
+// Swap to number without using third variable:
+
+// public class bitproblem {
+//     public static void main(String[] args) {
+//         int a = 5;
+//         int b = 7;
+
+//         a = a ^ b;
+//         b = a ^ b; // (a ^ b) ^ b = a;
+//         a = a ^ b; // (a ^ b) ^ a = b;
+//         // swapped successfully
+
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// }
+
+
+
+// Check if the ith bit is set or not:
+// public class bitproblem {
+//     public static void main(String[] args) {
+
+//         // Using left shift operator
+//         int i = 1;
+//         int n = 13;
+
+//         if ((n & (1 << i)) != 0) {
+//             System.out.println("Bit is set");
+//         } else {
+//             System.out.println("Bit is not set");
+//         }
+
+//         // Using right shift operator
+//         int j = 1;
+//         int x = 13;
+
+//         if (((x >> j) & 1) == 1) {
+//             System.out.println("Bit is set");
+//         } else {
+//             System.out.println("Bit is not set");
+//         }
+//     }
+// }
+
+
+
+// Setting an ith bit
