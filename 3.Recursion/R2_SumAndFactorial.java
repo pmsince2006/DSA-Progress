0507c47cbc60f374b2287f26bb65c8f4e52093db
Prince Mathur <princemathur1596@gmail.com>
2026-03-31 10:40:10 +0530

    Added sum and factorial methods

--- 3.Recursion/R2_SumAndFactorial.java
+++ 3.Recursion/R2_SumAndFactorial.java
@@ -29,4 +29,13 @@ public class first{
     //     }
     //    return n + sum(n - 1);
     // }
+
+
+  // Factorial of N using recursion
+    // static int fact(int n){
+    //     if(n == 0 || n == 1){
+    //         return 1;
+    //     }
+    //    return n * fact(n - 1);
+    // }
 }
