8168fe3ee8dd45de589fd5e7643066147ef4d8e0
Prince Mathur <princemathur1596@gmail.com>
2026-03-31 10:38:28 +0530

    Add R2_SumAndFactorial.java with recursion examples
    Created a new Java file for recursion examples including printing numbers from 1 to n and n to 1.

---
+++ 3.Recursion/R2_SumAndFactorial.java
@@ -0,0 +1,23 @@
+public class first{
+  public static void main(String[] args) {
+        System.out.println(print(5));
+    }
+  
+  // Printing 1 to n using recursion
+    // static void print(int n){
+    //     if(n == 0){
+    //         return;
+    //     }
+    //     print(n - 1);
+    //     System.out.println(n);
+    // }
+
+    // Printing n to 1 using recursion 
+    // static void print(int n){
+    //     if(n == 0){
+    //         return;
+    //     }
+    //     System.out.println(n);
+    //     print(n - 1);
+    // }
+}
