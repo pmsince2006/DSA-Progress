309752fb31f3ba044af57892bb8bced7844d586f
Prince Mathur <princemathur1596@gmail.com>
2026-03-31 10:42:25 +0530

    Add R3_ArraysAndPalindrome.java with array reversal function
    Added a function to reverse an array.

---
+++ 3.Recursion/R3_ArraysAndPalindrome.java
@@ -0,0 +1,20 @@
+public class first{
+  public static void main(String[] args) {
+        System.out.println();
+    }
+
+   //Function to reverse an array using two parameters
+    // static void rev(int[] arr, int start, int end){
+
+    //     if(start >= end){
+    //         return;
+    //     }
+
+    //     int temp = arr[start];
+    //     arr[start] = arr[end];
+    //     arr[end] = temp;
+
+    //     rev(arr, start + 1, end - 1);
+    // }
+  
+}
