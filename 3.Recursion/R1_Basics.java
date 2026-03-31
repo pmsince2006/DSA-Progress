cb1d6cf569cfb5896ff8827aa4a4bd708a2d582e
Prince Mathur <princemathur1596@gmail.com>
2026-03-31 10:35:38 +0530

    Implement main method to showcase recursion
    Added a main method to demonstrate recursion by printing a name multiple times.

--- 3.Recursion/R1_Basics.java
+++ 3.Recursion/R1_Basics.java
@@ -21,5 +21,18 @@
 // Time Complexity of Fibonacci (recursive):
 // O(2^n)
 public class first {
+    public static void main(String[] args) {
+        System.out.println(printn(5));
+        
+    }
+
+    // Printing name n times using recursion
+    //  static void printn(int n){
+    //     if(n == 0){
+    //         return;
+    //     }
+    //     printn(n-1);
+    //     System.out.println("Prins mathur");
+    // }
     
 }
