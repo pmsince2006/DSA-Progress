6ee469b46018c41f3884c133336433ade8d79b35
Prince Mathur <princemathur1596@gmail.com>
2026-03-31 10:44:29 +0530

    Added alternate palindrome method

--- 3.Recursion/R3_ArraysAndPalindrome.java
+++ 3.Recursion/R3_ArraysAndPalindrome.java
@@ -48,5 +48,21 @@ public class first{
     //     }
     //     return palindrome(s,i + 1);
     // }
-  
+
+
+
+   // Alternate approach using while loop
+    // static boolean isPalindrome(String str){
+    //     int i = 0;
+    //     int j = str.length() - 1;
+
+    //     while(i < j){
+    //         if(str.charAt(i) != str.charAt(j)){
+    //             return false;
+    //         }
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
 }
