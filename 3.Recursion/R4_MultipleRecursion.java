b301091658bb74e1a4015220112695bda0a030b9
Prince Mathur <princemathur1596@gmail.com>
2026-03-31 10:47:25 +0530

    Added Fibonacci series
    Add a Java class for Fibonacci sequence using recursion.

---
+++ 3.Recursion/R4_MultipleRecursion.java
@@ -0,0 +1,20 @@
+
+
+public class first{
+   public static void main(String[] args) {
+        System.out.println(fibo(5));
+    }
+
+     //multiple recursion calls
+    // Fibonacci Sequence
+    // static int fibo(int n){
+    //     if(n == 0){
+    //         return 0;
+    //     }
+    //     if(n == 1){
+    //         return 1;
+    //     }
+
+    //     return fibo(n - 1) + fibo(n -2);
+    // }
+}
