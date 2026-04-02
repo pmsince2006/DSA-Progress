c85acf46b99fe410525c9b41e0c23df91749ec15
Prince Mathur <princemathur1596@gmail.com>
2026-04-02 07:22:08 +0530

    Add Java program to print all subsequences of an array

---
+++ 3.Recursion/R5_Subsequences.java
@@ -0,0 +1,27 @@
+//Print all subsequence
+//Subsequence is contigous | non contingous sequence which allows the order 
+// Number of subsequence for an array with n elements will be 2^n
+
+public class first{
+  
+  public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        subsequences(arr, 0, new ArrayList<>());
+  }
+
+  static void subsequences(int[] arr, int i, ArrayList<Integer> list){
+    
+        if(i == arr.length){
+            System.out.println(list);
+            return;
+        }
+
+        list.add(arr[i]);
+        subsequences(arr, i + 1, list);
+
+        list.remove(list.size() - 1);
+
+        subsequences(arr, i + 1, list);
+    }
+  
+}
